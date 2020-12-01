7aaf427018336baf5e89364b577c1171fd313cb9
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-01 17:59:09 -0500

    Added task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 0; number <= 98; number++)
+            {
+                Console.WriteLine("{0} = 0x{1}", number, number.ToString("x"));
+            }
+        }
+    }
+}
