bfee5b2c459da328aa2e88d4d5ddd0cb90a3065d
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-01 18:48:05 -0500

    Added task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,14 @@
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c >= 'a' && c <= 'z')
+        {
+            return (true);
+        }
+        else
+        {
+            return (false);
+        }
+    }
+}
