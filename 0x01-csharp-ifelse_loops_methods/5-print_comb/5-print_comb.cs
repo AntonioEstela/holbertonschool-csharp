38b9dc8b19df40a7280ee23b70069208eaaae1dc
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-01 18:09:12 -0500

    Added task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 0; number < 100; number++)
+            {
+                if (number < 99)
+                {
+                    Console.Write($"{number:00}, ");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
