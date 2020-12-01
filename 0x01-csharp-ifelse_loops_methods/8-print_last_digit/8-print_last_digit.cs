08d960fda4a84c2efa7af6e8e06eaa6a6000ef90
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-01 18:56:05 -0500

    Added task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = Math.Abs(number % 10);
+        
+        Console.Write(lastDigit);
+
+        return (lastDigit);
+    }
+}
