fca84a175ef6e48334c759ad01062cbf477ab792
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-01 18:31:19 -0500

    Added task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int first = 0; first <= 9; first++)
+            {
+                for (int second = 1; second <= 9; second++)
+                {
+                    if (second > first)
+                    {
+                        Console.Write($"{first}{second}");
+                        if (first < 8)
+                        {
+                            Console.Write(", ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
