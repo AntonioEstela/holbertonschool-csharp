0ed0aaac5aeb8ce7b3565f2e5f1982058c81e236
Antonio Estela <antonioestela73@gmail.com>
2020-12-02 17:54:45 -0500

    Added task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[,] {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(array2D[i, j]);
+                    if (j < array2D.Length)
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
