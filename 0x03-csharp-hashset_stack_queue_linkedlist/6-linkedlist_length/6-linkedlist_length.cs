a2def39c2c25a11775b6050df06fd530666529ef
Antonio Estela <antonioestela73@gmail.com>
2020-12-03 16:11:10 -0500

    Added task 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int length = 0;
+
+        foreach (var item in myLList)
+            length += 1;
+
+        return (length);
+    }
+}
