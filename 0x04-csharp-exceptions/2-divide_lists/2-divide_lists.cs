000b1c4fb6f650a7b696ff4e9ac7b69a653df4ea
Antonio Estela <antonioestela73@gmail.com>
2020-12-14 12:53:25 -0500

    Fixed task 2

--- 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -5,10 +5,9 @@ class List
 {
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
-        int length = list1.Count > list2.Count ? list1.Count : list2.Count;
         List<int> newList = new List<int>();
 
-        for (int i = 0; i < length; i++)
+        for (int i = 0; i < listLength; i++)
         {
             try
             {
