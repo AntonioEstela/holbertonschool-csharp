76d5fb67158e5c37e51631ee04dc2c7f750cac64
Antonio Estela <antonioestela73@gmail.com>
2020-12-15 12:41:39 -0500

    Added task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,15 @@
+namespace Enemies
+{
+    /// <summary>Provides a new Zombie enemy type.</summary>
+    public class Zombie
+    {
+        /// <summary>Int that represents the value of the Zombie health</summary>
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/>class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
