56ea3ac19595808d5fb900a8f8dcb9122cb94fa0
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-17 13:12:42 -0500

    Added task 0

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [Test]
+        public void Test_adding_two_positive_integers()
+        {
+            int a = 4;
+            int b = 5;
+            Assert.AreEqual(MyMath.Operations.Add(a, b), 9);
+        }
+
+        [Test]
+        public void Test_adding_two_negative_integers()
+        {
+            Assert.AreEqual(MyMath.Operations.Add(-5, -15), -20);
+        }
+
+        [TestCase(5, 9)]
+        [TestCase(20, -50)]
+        [TestCase(1, 1)]
+        [TestCase(0, 0)]
+
+        [Test]
+        public void Test_adding_two_integers(int a, int b)
+        {
+            Assert.AreEqual(MyMath.Operations.Add(a, b), a + b);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>Provides some math operations</summary>
+    public class Operations
+    {
+        /// <summary>Returns the result of add 2 numbers</summary>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
