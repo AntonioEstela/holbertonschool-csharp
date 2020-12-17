7b082508b0df814ba4400648f7a15117ce097c3e
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-17 16:43:58 -0500

    Added task 2

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    /// <summary>Provides testscases for MaxInteger method.</summary>
+    public class MaxIntegerTests
+    {
+        [Test]
+        public void Test_when_all_positive()
+        {
+            List<int> intList = new List<int>() {1, 2, 3, 9, 5, 6, 23};
+
+            Assert.AreEqual(MyMath.Operations.Max(intList), 23);
+        }
+
+        [Test]
+        public void Test_when_all_negative()
+        {
+            List<int> intList = new List<int>() {-1, -2, -3, -9, -5, -6, -23};
+
+            Assert.AreEqual(MyMath.Operations.Max(intList), -1);
+        }
+
+        
+        [Test]
+        public void Test_when_all_equals()
+        {
+            List<int> intList = new List<int>() {3, 3, 3, 3, 3};
+
+            Assert.AreEqual(MyMath.Operations.Max(intList), 3);
+        }
+
+        [Test]
+        public void Test_when_list_empty()
+        {
+            Assert.AreEqual(MyMath.Operations.Max(null), 0);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyMath
+{
+    /// <summary>Provides operations with integers</summary>
+    public class Operations
+    {
+
+        /// <summary>Find the max integer in a list</summary>
+        public static int Max(List<int> nums)
+        {
+            if (nums == null)
+                return 0;
+
+            int maxInt = nums.First();
+
+            foreach (var item in nums)
+            {
+                if (item > maxInt)
+                    maxInt = item;
+            }
+
+            return maxInt;
+        }
+    }
+}
