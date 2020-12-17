5e39aa0f9fcb19609ee6139b14c1a0030709a5f7
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-17 18:01:53 -0500

    Added task 3

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Text
+{
+    /// <summary>Provides methods to work with strings.</summary>
+    public class Str
+    {
+        /// <summary>Checks if a srting is palindrome or not.</summary>
+        /// <returns>True if a string is palindrome, otherwhise False.</returns>
+        public static bool IsPalindrome(string s)
+        {
+            StringBuilder newString = new StringBuilder();
+            string lower;
+
+            foreach (char item in s)
+            {
+                if (!Char.IsPunctuation(item))
+                {
+                    if (!Char.IsWhiteSpace(item))
+                    {
+                        newString.Append(item);
+                    }
+                }
+            }
+
+            lower = newString.ToString().ToLower();
+
+            int j = lower.Length - 1;
+            for (int i = 0; i < lower.Length; i++, j--)
+            {
+                if (lower[i] != lower[j])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
