a9faa6a784e44403ceb7f05d1f842bf9b27a26b6
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-19 18:54:46 -0500

    Changed method to get the type of the object

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -6,6 +6,6 @@ class Obj
     /// <summary>Returns True if the object is an int, otherwise return False.</summary>
     public static bool IsOfTypeInt(object obj)
     {
-        return obj is int;
+        return obj.GetType() == typeof(int);
     }
 }
