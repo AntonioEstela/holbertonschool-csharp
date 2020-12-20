ae7764dca636b55240c7da829229d38a5669349e
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-20 00:05:59 -0500

    Added task 6

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,51 @@
+using System;
+
+/// <summary>Shape base class</summary>
+class Shape
+{
+
+    /// <summary>Not implemented class that should return the area of the shape</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>Rectangle class that inherits from shape base class.</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get
+        {
+            return this.width;
+        }
+
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+
+            else
+                this.width = value;
+        }
+    }
+    public int Height
+    {
+        get
+        {
+            return this.height;
+        }
+
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+
+            else
+                this.height = value;
+        }
+    }
+}
