8518d76bb0c99bb094cf8c810f37a676215f206d
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2020-12-19 23:13:26 -0500

    Added some XML documentation

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 
+/// <summary>Provides some methods to work with Obj</summary>
 class Obj
 {
+    ///<summary>
+    /// method that returns True if the object is an instance of,
+    /// or if the object is an instance of a class that inherited from, Array, otherwise return False.
+    ///</summary>
     public static bool IsInstanceOfArray(object obj)
     {
         return obj is Array;
