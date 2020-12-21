ebd03940f8245bab3fa9e556438ced5307722632
Antonio Estela <antonioestela73@gmail.com>
2020-12-20 20:02:30 -0500

    Fixed documentation

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 /// <summary>Provides some methods to work with Obj</summary>
 class Obj
 {
-    ///<summary>
-    /// method that returns True if the object is an instance of,
-    /// or if the object is an instance of a class that inherited from, Array, otherwise return False.
-    ///</summary>
+    ///<summary>Method that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, Array, otherwise return False.</summary>
     public static bool IsInstanceOfArray(object obj)
     {
         return obj is Array;
