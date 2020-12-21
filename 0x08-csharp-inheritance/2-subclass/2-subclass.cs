e787dce4bc25c70772b9d00c1a21620765fff6c0
Antonio Estela <antonioestela73@gmail.com>
2020-12-20 20:15:13 -0500

    Fixed documentation

--- 0x08-csharp-inheritance/2-subclass/2-subclass.cs
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -3,10 +3,7 @@ using System;
 /// <summary>Provides some methods to work with Obj</summary>
 class Obj
 {
-    /// <summary>
-    ///  Method that returns True if the object is an instance of a class
-    ///  that inherits from the specified class, otherwise return False
-    ///</summary>
+    /// <summary>Method that checks if the object is an instance of a class that inherits from the specified class.</summary>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
         return derivedType.BaseType != baseType.BaseType;
