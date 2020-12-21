e36d2c0fa59fc633d82b24ca47ba8742e18459b7
Antonio Estela <antonioestela73@gmail.com>
2020-12-20 20:19:47 -0500

    Fixed task 2

--- 0x08-csharp-inheritance/2-subclass/2-subclass.cs
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -6,6 +6,6 @@ class Obj
     /// <summary>Method that checks if the object is an instance of a class that inherits from the specified class.</summary>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
-        return derivedType.BaseType != baseType.BaseType;
+        return derivedType.IsSubclassOf(baseType);
     }
 }
