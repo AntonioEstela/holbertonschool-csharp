6822ff5c6bae84642f8b8939b02987743971f515
Antonio Estela <antonioestela73@gmail.com>
2021-01-20 19:46:50 -0500

    I fixed it

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -6,10 +6,10 @@ class VectorMath
     /// <summary>Calculates dot product of either two 2D or two 3D vectors</summary>
     public static double DotProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length > 3 || vector2.Length > 3)
+        if (vector1.Length > 3 || vector2.Length > 3 || vector1.Length != vector2.Length)
             return -1;
 
-        if (vector1.Length < 2 || vector2.Length < 2)
+        if (vector1.Length < 2 || vector2.Length < 2 || vector1.Length != vector2.Length)
             return -1;
 
         double result = new double();
