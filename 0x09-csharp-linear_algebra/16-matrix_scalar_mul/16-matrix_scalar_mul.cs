9d83288198f9c980b7dd75fa8fa2212ae5290a42
Antonio Estela <antonioestela73@gmail.com>
2021-01-21 11:34:38 -0500

    Upload task 16

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,22 @@
+using System;
+
+/// <summary>Multiplies a matrix and a scalar and returns the resulting matrix</summary>
+class MatrixMath
+{
+    /// <summary>Multiplies a matrix and a scalar and returns the resulting matrix</summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.GetLength(0) > 3 || matrix.GetLength(0) < 2)
+            return new double[,] { {-1} };
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] *= scalar;
+            }
+        }
+
+        return matrix;
+    }
+}
