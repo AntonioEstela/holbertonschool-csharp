a5c95f1c725c2b158892674e380a9f092ead7ce9
Antonio Estela <antonioestela73@gmail.com>
2021-01-21 11:43:10 -0500

    Attempting to fix task 16

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -9,6 +9,9 @@ class MatrixMath
         if (matrix.GetLength(0) > 3 || matrix.GetLength(0) < 2)
             return new double[,] { {-1} };
 
+        if (matrix.GetLength(1) > 3 || matrix.GetLength(1) < 2)
+            return new double[,] { {-1} };
+
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(0); j++)
