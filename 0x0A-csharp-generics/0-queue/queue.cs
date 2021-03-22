b12b2a1bf3cdfd9f107b66e42e0f87c5d8aa9f28
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2021-03-22 12:21:37 -0500

    First upload

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Generic class Queue</summary>
+public class Queue<T>
+{
+    /// <summary>Function that returns the type of the instance</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
