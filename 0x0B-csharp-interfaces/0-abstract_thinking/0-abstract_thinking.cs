90eec09aa70e68c73e3588aa856c3b8b7de81aed
Antonio Estela <antonioestela73@gmail.com>
2021-03-23 13:17:45 -0500

    First upload

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+using System;
+
+/// <summary>Abstract class called Base</summary>
+public abstract class Base
+{
+    /// <summary>name is a public property</summary>
+    public string name;
+
+    /// <summary>Function that overrides the ToString method</summary>
+    public override string ToString()
+    {
+        return $"{this.name} is {this.GetType().Name}";
+    }
+}
