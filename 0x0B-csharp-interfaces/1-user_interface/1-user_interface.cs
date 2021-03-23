d65afa5abeabe1890721d5fb46fa7804b84e4e59
Antonio Estela <antonioestela73@gmail.com>
2021-03-23 14:09:34 -0500

    First upload for task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,63 @@
+using System;
+
+/// <summary>Abstract class called Base</summary>
+public abstract class Base
+{
+    /// <summary>name is a public property</summary>
+    public string name;
+
+    /// <summary>Function that overrides the ToString meth</summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType().Name}";
+    }
+}
+
+/// <summary>Interface IInteractive</summary>
+public interface IInteractive
+{
+    /// <summary>Method Interact</summary>
+    void Interact();
+}
+
+/// <summary>Interface IBreakable</summary>
+public interface IBreakable
+{
+    /// <summary>property durability</summary>
+    int durability { get; set; }
+
+    /// <summary>Method Break</summary>
+    void Break();
+}
+
+/// <summary>Interface ICollectable</summary>
+public interface ICollectable
+{
+    /// <summary>property isCollected</summary>
+    bool isCollected { get; set; }
+
+    /// <summary>Method Collect</summary>
+    void Collect();
+}
+
+/// <summary>Class TestObject</summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    /// <summary>property durability</summary>
+    public int durability { get; set; }
+
+    /// <summary>property isCollected</summary>
+	public bool isCollected { get; set; }
+
+    /// <summary>Method Interact</summary>
+	public void Interact()
+	{ }
+
+    /// <summary>Method Break</summary>
+	public void Break()
+	{ }
+
+    /// <summary>Method Collect</summary>
+	public void Collect()
+	{ }
+}
